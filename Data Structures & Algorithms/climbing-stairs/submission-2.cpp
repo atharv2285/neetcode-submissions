78ee37a499dd9d3a388a47785d3e76ef94856992
PEACHMAN <143731767+atharv2285@users.noreply.github.com>
2026-07-28 19:30:07 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int climbStairs(int n) 
+    {
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+        vector<int> res(n+1);
+        res[1] = 1;
+        res[2] = 2;
+
+        for(int i=3; i<=n; i++)
+        {
+            res[i] = res[i-1]+res[i-2];
+        }    
+
+        return res[n];
+        
+    }
+};
