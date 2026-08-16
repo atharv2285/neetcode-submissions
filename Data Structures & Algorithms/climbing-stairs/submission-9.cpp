90375f76dbe730a4aa9f625d7737fb28539806c2
PEACHMAN <143731767+atharv2285@users.noreply.github.com>
2026-08-16 16:01:28 +0530

    Add: climbing-stairs - submission-9

---
+++ Data Structures & Algorithms/climbing-stairs/submission-9.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+vector<int> dp;
+    int climbStairs(int n) 
+    {
+        if(n==1) return 1;
+        dp.resize(n+1,-1);
+        dp[1]=1;
+        dp[2]=2;
+        return dfs(n);
+
+        
+    }
+
+    int dfs(int i)
+    {
+        if(dp[i]!=-1) return dp[i];
+        dp[i] = dfs(i-1) + dfs(i-2);
+        return dp[i];
+    }
+};
