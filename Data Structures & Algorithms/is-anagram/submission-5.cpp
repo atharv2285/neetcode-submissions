765a2da28b026a953a985b6426aa0cf6860b3e73
PEACHMAN <143731767+atharv2285@users.noreply.github.com>
2026-08-27 20:28:21 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) 
+    {
+        if(s.size()!=t.size()) return false;
+        unordered_map <char,int> mp;
+
+        for(char c: s)
+        {
+            mp[c]++;
+        }
+
+        for(char c: t)
+        {
+            if(mp.count(c) && mp[c]>0)
+            {
+                mp[c]--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;    
+    }
+};
